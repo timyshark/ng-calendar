510eb92cef833da2040dc11af4f9d686e552af84
Edudater <edudate@edudate.academy>
2023-04-16 09:30:48 -0700

    Initial Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarHeaderComponent } from './calendar-header/calendar-header.component';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FlatpickrModule } from 'angularx-flatpickr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarHeaderComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    BrowserModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
